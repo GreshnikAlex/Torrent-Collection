dd7ab9dfd49663b104ddb9662d14f35086dd3c9e
Александр <1995BLADE@mail.ru>
2018-06-08 21:30:50 +0300

    Добавление команды кнопкам "Регистрация" и "Отмена"

---
+++ Torrent Collection/ViewModel/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Torrent_Collection.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Метод команды для клика на кнопку "Регистрация"
+        /// </summary>
+        public RelayCommand Reg_Click => new RelayCommand(obj =>
+        {
+            new View.RegistrationView().ShowDialog();
+        });
+
+        //INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+    }
+}
---
+++ Torrent Collection/ViewModel/RegistrationViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace Torrent_Collection.ViewModel
+{
+    public class RegistrationViewModel : INotifyPropertyChanged
+    {
+        private Window window;
+
+        public RegistrationViewModel(Window window)
+        {
+            this.window = window;
+        }
+
+        /// <summary>
+        /// Метод команды для клика на кнопку "Отмена"
+        /// </summary>
+        public RelayCommand Cancel_Click => new RelayCommand(obj =>
+        {
+            window.Close();
+        });
+
+        //INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+    }
+}
